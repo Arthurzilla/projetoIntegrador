562875c8dba40824024fb1418987a78b84b2791e
Arthur <arthurcardoso693@gmail.com>
2023-09-30 18:53:34 -0300

    first commit

---
+++ GerenciamentoDidatico/LançarFaltas/script.js
@@ -0,0 +1,5 @@
+function alerta(){
+    alert("faltas enviadas!")
+}
+var button = document.getElementById('button-pogger')
+button.addEventListener('click', alerta)
---
+++ Login/script.js
@@ -0,0 +1,22 @@
+function passwordVerification(){
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "" || password == ""){
+      document.getElementById('error').textContent = "há espaços em branco"
+    }else if(username != "arthur" || password != "332518"){
+      document.getElementById('error').textContent = "senha ou usuários incorretos"
+    }else{
+      pageRedirect()
+    }
+
+  }
+
+
+  function pageRedirect(){
+    window.location.href="../GerenciamentoDidatico/index.html"
+  }
+
+  var button = document.getElementById('submit')
+  button.addEventListener('click', passwordVerification)
